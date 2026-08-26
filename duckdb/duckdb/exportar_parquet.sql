92cf9b188161657818f3ba58f3ffff65874cacfe
suzanchristoff <suprofissional@gmail.com>
2026-08-26 17:11:59 -0300

    Delete duckdb/duckdb directory

--- duckdb/duckdb/exportar_parquet.sql
+++
@@ -1,3 +0,0 @@
-COPY gold.base_analise TO 'dados/dados_prontos/base_analise.parquet' (FORMAT PARQUET);
-COPY gold.segmentos_risco TO 'dados/dados_prontos/segmentos_risco.parquet' (FORMAT PARQUET);
-COPY gold.score_clientes TO 'dados/dados_prontos/score_clientes.parquet' (FORMAT PARQUET);
